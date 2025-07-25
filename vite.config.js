20172d530944573ca368a0e368e1f9db4dcb208c
Gustavo Mendez <gadolfomendez@outlook.es>
2025-07-25 13:56:40 -0300

    actualización

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Che-tape/',  // poné el nombre exacto de tu repo en GitHub
-  plugins: [react(), vitePluginGhPages()],
+  plugins: [react()],
 });
